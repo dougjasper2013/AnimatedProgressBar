c1bf8eaff0dcb04985897c8df2811f889bb56fd3
Douglas Jasper <doug.jasper@trios.com>
2025-03-17 08:14:29 -0400

    Initial Commit

---
+++ AnimatedProgressBar/AnimatedProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedProgressBarApp.swift
+//  AnimatedProgressBar
+//
+//  Created by Douglas Jasper on 2025-03-17.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
