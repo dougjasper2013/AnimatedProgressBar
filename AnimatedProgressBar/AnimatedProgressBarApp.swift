9c1396f929999079c166883ab6fa8d41be3dc365
Douglas Jasper <doug.jasper@trios.com>
2025-10-28 10:09:48 -0400

    Initial Commit

---
+++ AnimatedProgressBar/AnimatedProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedProgressBarApp.swift
+//  AnimatedProgressBar
+//
+//  Created by Douglas Jasper on 2025-10-28.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedProgressBarTests/AnimatedProgressBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedProgressBarTests.swift
+//  AnimatedProgressBarTests
+//
+//  Created by Douglas Jasper on 2025-10-28.
+//
+
+import Testing
+@testable import AnimatedProgressBar
+
+struct AnimatedProgressBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
